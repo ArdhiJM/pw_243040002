f1d151cb5756020844c7045b72d050342a19c20f
ArdhiJM <ardhijamilm0987@gmail.com>
2025-02-13 20:49:41 +0700

    finish modul 1

--- kuliah/pertemuan1/latihan1.php
+++ kuliah/pertemuan1/latihan1.php
@@ -18,9 +18,10 @@ echo "Ardhi Jamil M";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar PHP</title>
+    <title>Latihan 1 PHP</title>
 </head>
 <body>
-
+    <h1><?php echo "Hello World"; ?></h1>
+    <p>Hello nama saya <?php echo "Ardhi" ?></p>
 </body>
 </html>
---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+    $nama = "Srdhi Jamil M";
+    $email = "ardhijamilm7890@gmail.com";
+    $pekerjaan = "Mahasiswa";
+    $jurusan = "Informatika";
+    $universitas = "Universitas Pasundan";
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email</h4>";
+    echo "<p>Halo, Nama saya $nama, Saya adalah $pekerjaan Jurusan $jurusan di $universitas</p>";
+?>
