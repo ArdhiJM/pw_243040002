d5a129f221790cf6555dd63ea927a5621c34909c
ArdhiJM <ardhijamilm0987@gmail.com>
2025-02-13 20:09:30 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// Pertemuan 2 - PHP Dasar 
+// Sintaks PHP
+
+// Standar Output
+// echo, print 
+// print_r
+// var_dump isi dari variabel
+
+// penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP ( tidak disarankan )
+echo "Ardhi Jamil M";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+
+</body>
+</html>
